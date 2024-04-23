13150f29c3c08b9ea023f32c7c0ae8ee072d45b6
rory-beals <rtba2021@mymail.pomona.edu>
2024-04-23 10:52:55 +0200

    DataPreprocess is messy

--- DataPreprocess.py
+++ DataPreprocess.py
@@ -2,6 +2,7 @@
 from collections import defaultdict
 import os
 import re
+import cv2
 import tensorflow as tf
 import torch
 import matplotlib.pyplot as plt
@@ -48,6 +49,81 @@ print('hi3')
 image_processor = SegformerImageProcessor(do_reduce_labels=True)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# just kinda testing this out and figuring out if it works
+image = Image.open("/Users/rorybeals/Downloads/Road Identification Data/test/206_sat.jpg")
+png_image = Image.open("/Users/rorybeals/Downloads/Road Identification Data/train/562_mask.png")
+model.to(device)
+pixel_vals = image_processor(image, return_tensors="pt").pixel_values.to(device)
+
+# forward pass
+with torch.no_grad():
+  outputs = model(pixel_values=pixel_vals)
+
+# took this from online to help with visualizing the segmentation
+def ade_palette():
+    """ADE20K palette that maps each class to RGB values."""
+    return [[120, 120, 120], [180, 120, 120], [6, 230, 230], [80, 50, 50],
+            [4, 200, 3], [120, 120, 80], [140, 140, 140], [204, 5, 255],
+            [230, 230, 230], [4, 250, 7], [224, 5, 255], [235, 255, 7],
+            [150, 5, 61], [120, 120, 70], [8, 255, 51], [255, 6, 82],
+            [143, 255, 140], [204, 255, 4], [255, 51, 7], [204, 70, 3],
+            [0, 102, 200], [61, 230, 250], [255, 6, 51], [11, 102, 255],
+            [255, 7, 71], [255, 9, 224], [9, 7, 230], [220, 220, 220],
+            [255, 9, 92], [112, 9, 255], [8, 255, 214], [7, 255, 224],
+            [255, 184, 6], [10, 255, 71], [255, 41, 10], [7, 255, 255],
+            [224, 255, 8], [102, 8, 255], [255, 61, 6], [255, 194, 7],
+            [255, 122, 8], [0, 255, 20], [255, 8, 41], [255, 5, 153],
+            [6, 51, 255], [235, 12, 255], [160, 150, 20], [0, 163, 255],
+            [140, 140, 140], [250, 10, 15], [20, 255, 0], [31, 255, 0],
+            [255, 31, 0], [255, 224, 0], [153, 255, 0], [0, 0, 255],
+            [255, 71, 0], [0, 235, 255], [0, 173, 255], [31, 0, 255],
+            [11, 200, 200], [255, 82, 0], [0, 255, 245], [0, 61, 255],
+            [0, 255, 112], [0, 255, 133], [255, 0, 0], [255, 163, 0],
+            [255, 102, 0], [194, 255, 0], [0, 143, 255], [51, 255, 0],
+            [0, 82, 255], [0, 255, 41], [0, 255, 173], [10, 0, 255],
+            [173, 255, 0], [0, 255, 153], [255, 92, 0], [255, 0, 255],
+            [255, 0, 245], [255, 0, 102], [255, 173, 0], [255, 0, 20],
+            [255, 184, 184], [0, 31, 255], [0, 255, 61], [0, 71, 255],
+            [255, 0, 204], [0, 255, 194], [0, 255, 82], [0, 10, 255],
+            [0, 112, 255], [51, 0, 255], [0, 194, 255], [0, 122, 255],
+            [0, 255, 163], [255, 153, 0], [0, 255, 10], [255, 112, 0],
+            [143, 255, 0], [82, 0, 255], [163, 255, 0], [255, 235, 0],
+            [8, 184, 170], [133, 0, 255], [0, 255, 92], [184, 0, 255],
+            [255, 0, 31], [0, 184, 255], [0, 214, 255], [255, 0, 112],
+            [92, 255, 0], [0, 224, 255], [112, 224, 255], [70, 184, 160],
+            [163, 0, 255], [153, 0, 255], [71, 255, 0], [255, 0, 163],
+            [255, 204, 0], [255, 0, 143], [0, 255, 235], [133, 255, 0],
+            [255, 0, 235], [245, 0, 255], [255, 0, 122], [255, 245, 0],
+            [10, 190, 212], [214, 255, 0], [0, 204, 255], [20, 0, 255],
+            [255, 255, 0], [0, 153, 255], [0, 41, 255], [0, 255, 204],
+            [41, 0, 255], [41, 255, 0], [173, 0, 255], [0, 245, 255],
+            [71, 0, 255], [122, 0, 255], [0, 255, 184], [0, 92, 255],
+            [184, 255, 0], [0, 133, 255], [255, 214, 0], [25, 194, 194],
+            [102, 255, 0], [92, 0, 255]]
+     
+predicted_segmentation_map = image_processor.post_process_semantic_segmentation(outputs, target_sizes=[image.size[::-1]])[0]
+predicted_segmentation_map = predicted_segmentation_map.cpu().numpy()
+print(predicted_segmentation_map)
+
+color_seg = np.zeros((predicted_segmentation_map.shape[0],
+                      predicted_segmentation_map.shape[1], 3), dtype=np.uint8) # height, width, 3
+
+palette = np.array(ade_palette())
+for label, color in enumerate(palette):
+    color_seg[predicted_segmentation_map == label, :] = color
+# Convert to BGR
+color_seg = color_seg[..., ::-1]
+# cv2.imwrite("image_out.jpg", color_seg)
+
+# Show image + mask
+img = np.array(image) * 0.5 + color_seg * 0.5
+img = img.astype(np.uint8)
+
+plt.figure(figsize=(15, 10))
+plt.imshow(img)
+plt.show()
+
+print('ahhhhh')
 # this function is supposed to process the png files and encode them with proper values that we can use to retrain the model a bit
 def process_png(filename):
     image = Image.open(filename)
@@ -85,7 +161,7 @@ for file in jpg_files:
     png_image = data_dict[file_num]['mask.png']
     png_filename = path + '/'+  png_image
     training_labels.append(png_filename)
-    if (len(training_images) == 1000):
+    if (len(training_images) == 20):
         break
 
 processed_images = []
@@ -114,28 +190,26 @@ training_labels_tensor = tf.convert_to_tensor(training_labels)
 print('hi5')
 
 # define optimizer
-optimizer = torch.optim.AdamW(model.parameters(), lr=0.00006)
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.01)
 image_processor = SegformerImageProcessor(do_reduce_labels=True)
 model.to(device)
 # tune the model
 print('hi6')
 model.train()
-for epoch in range(200):  # loop over the dataset multiple times
+for epoch in range(2):  # loop over the dataset multiple times
     print("Epoch:", epoch)
     for batch in dataloader:
+        print('batch running')
         # get the inputs;
         pixel_values, labels = batch
         pixel_values = pixel_values.to(device)
         pixel_values = pixel_values[:, 0, :, :, :]
         labels = labels.to(device)
-        print('gabagoo')
         # zero the parameter gradients
-        optimizer.zero_grad()
-        print('gabagoo2')
+        # optimizer.zero_grad()
         # forward + backward + optimize
         outputs = model(pixel_values=pixel_values, labels=labels)
         loss, logits = outputs.loss, outputs.logits
-        print('gabagoo3')
 
         loss.backward()
         optimizer.step()
@@ -154,7 +228,39 @@ for epoch in range(200):  # loop over the dataset multiple times
                   ignore_index=255,
                   reduce_labels=False, # we've already reduced the labels ourselves
               )
+        print('batch over')
 
     print("Loss:", loss.item())
     print("Mean_iou:", metrics["mean_iou"])
     print("Mean accuracy:", metrics["mean_accuracy"])
+
+# just kinda testing this out and figuring out if it works
+image = Image.open("/Users/rorybeals/Downloads/Road Identification Data/test/206_sat.jpg")
+png_image = Image.open("/Users/rorybeals/Downloads/Road Identification Data/train/562_mask.png")
+pixel_vals = image_processor(image, return_tensors="pt").pixel_values.to(device)
+
+# forward pass
+with torch.no_grad():
+  outputs = model(pixel_values=pixel_vals)
+
+     
+predicted_segmentation_map = image_processor.post_process_semantic_segmentation(outputs, target_sizes=[image.size[::-1]])[0]
+predicted_segmentation_map = predicted_segmentation_map.cpu().numpy()
+print(predicted_segmentation_map)
+
+color_seg = np.zeros((predicted_segmentation_map.shape[0],
+                      predicted_segmentation_map.shape[1], 3), dtype=np.uint8) # height, width, 3
+
+palette = np.array(ade_palette())
+for label, color in enumerate(palette):
+    color_seg[predicted_segmentation_map == label, :] = color
+# Convert to BGR
+color_seg = color_seg[..., ::-1]
+
+# Show image + mask
+img = np.array(image) * 0.5 + color_seg * 0.5
+img = img.astype(np.uint8)
+
+plt.figure(figsize=(15, 10))
+plt.imshow(img)
+plt.show()
